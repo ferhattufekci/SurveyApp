9fc5c7c5beb0dd39605b94183c148fea626ead26
Ferhat Tüfekçi <ferha@surveyapp.com>
2026-03-10 07:43:23 +0000

    feat(application): add service interfaces for all use cases

---
+++ Backend/SurveyApp.Application/Interfaces/IServices.cs
@@ -0,0 +1,58 @@
+using SurveyApp.Application.DTOs;
+
+namespace SurveyApp.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<LoginResponse?> LoginAsync(LoginRequest request);
+    string GenerateToken(int userId, string email, string role);
+}
+
+public interface IAnswerTemplateService
+{
+    Task<List<AnswerTemplateDto>> GetAllAsync();
+    Task<AnswerTemplateDto?> GetByIdAsync(int id);
+    Task<AnswerTemplateDto> CreateAsync(CreateAnswerTemplateRequest request);
+    Task<AnswerTemplateDto?> UpdateAsync(int id, UpdateAnswerTemplateRequest request);
+    Task<bool> DeleteAsync(int id);
+}
+
+public interface IQuestionService
+{
+    Task<List<QuestionListDto>> GetAllAsync();
+    Task<QuestionDto?> GetByIdAsync(int id);
+    Task<QuestionDto> CreateAsync(CreateQuestionRequest request);
+    Task<QuestionDto?> UpdateAsync(int id, UpdateQuestionRequest request);
+    Task<bool> DeleteAsync(int id);
+}
+
+public interface ISurveyService
+{
+    Task<List<SurveyListDto>> GetAllAsync();
+    Task<SurveyDetailDto?> GetByIdAsync(int id);
+    Task<SurveyDetailDto> CreateAsync(CreateSurveyRequest request);
+    Task<SurveyDetailDto?> UpdateAsync(int id, UpdateSurveyRequest request);
+    Task<bool> DeleteAsync(int id);
+}
+
+public interface IUserSurveyService
+{
+    Task<List<UserSurveyListDto>> GetAssignedSurveysAsync(int userId);
+    Task<SurveyDetailDto?> GetSurveyForUserAsync(int surveyId, int userId);
+    Task<bool> SubmitSurveyAsync(int userId, SubmitSurveyRequest request);
+}
+
+public interface IReportService
+{
+    Task<SurveyReportDto?> GetSurveyReportAsync(int surveyId);
+    Task<List<SurveyListDto>> GetAllSurveysWithStatsAsync();
+}
+
+public interface IUserService
+{
+    Task<List<UserDto>> GetAllAsync();
+    Task<UserDto?> GetByIdAsync(int id);
+    Task<UserDto> CreateAsync(CreateUserRequest request);
+    Task<UserDto?> UpdateAsync(int id, UpdateUserRequest request);
+    Task<bool> DeleteAsync(int id);
+}
